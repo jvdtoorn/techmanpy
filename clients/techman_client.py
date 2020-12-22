2b80901db3cd16fe6c5d2aa887cd55007734f1cf
Jules van der Toorn <J.vanderToorn@student.tudelft.nl>
2020-12-22 16:04:56 +0100

    Fixed bug with streamreader

--- clients/techman_client.py
+++ clients/techman_client.py
@@ -37,42 +37,57 @@ class TechmanClient:
 
    def send(self, techman_packet, callback=None):
       res = self._loop.run_until_complete(self._send(techman_packet, callback=callback))
-      if res is not None and isinstance(res, Exception):
+      if callback is None and isinstance(res, Exception):
          raise TechmanException(str(res) + ' (' + type(res).__name__ + ') ')
       else: return res
 
    async def _send(self, techman_packet, callback=None):
-      if not self.is_connected: await self._connect_async()
+      # Setup synchronous callback
       run_async = callback is not None
       res_future = asyncio.Future()
       def sync_callback(packet): res_future.set_result(packet)
       if not run_async: callback = sync_callback
-      await self._handle_outgoing(techman_packet, callback if run_async else sync_callback)
-      if not run_async: return await res_future
 
-   async def _handle_incoming(self, packet):
-      assert len(self._outgoing_requests) == 1
+      # Run class-specific queue setup
+      self._handle_outgoing(techman_packet, callback if run_async else sync_callback)
+
+      if not self.is_connected: await self._connect_async()
+      # Send message if connected
+      if self.is_connected:
+         self._writer.write(techman_packet.encoded())
+         if not run_async: return await res_future
+      # Send exception if not connected
+      else:
+         self._handle_exception(self._conn_exception)
+
+   def _handle_exception(self, exc):
+      for request in self._outgoing_requests:
+         request[1](exc)
+      self._outgoing_requests = []
+
+   def _handle_incoming(self, packet):
+      if len(self._outgoing_requests) == 0: return
       callback = self._outgoing_requests[0][1]
       self._outgoing_requests = []
       # Execute callback with received packet
       callback(packet)
 
-   async def _handle_outgoing(self, packet, callback):
+   def _handle_outgoing(self, packet, callback):
       assert len(self._outgoing_requests) == 0
       self._outgoing_requests = [(None, callback)]
-      if self.is_connected: self._writer.write(packet.encoded())
-      else: await self._handle_incoming(self._conn_exception)
 
    async def _listen(self):
       while True:
          if not self.is_connected:
             if not await self._connect_async():
                # Notify requests that no connection is in place
-               if len(self._outgoing_requests) > 0: await self._handle_incoming(self._conn_exception)
+               if len(self._outgoing_requests) > 0: self._handle_exception(self._conn_exception)
                print('[TechmanClient] WARN: Not connected. Trying again in 3 seconds')
                await asyncio.sleep(3)
                continue
-         read_bytes = await self._reader.read(1024)
+         read_bytes = await self._reader.read(100000)
+          # Ignore broken message (doesn't start with '$' or doesnt end with checksum)
+         if read_bytes[0] != 0x24 or read_bytes[-5] != 0x2A: continue
          if read_bytes == b'':
             if self._reconnect_cnt > 5:
                self._reconnect_cnt = 0
@@ -85,8 +100,8 @@ class TechmanClient:
          res = TechmanPacket(read_bytes)
          if res._header == 'CPERR':
             print('[TechmanClient] ERROR: %s' % CPERR_packet(res).description)
-            raise TechmanException(CPERR_packet(res).description)
-         await self._handle_incoming(res)
+            self._handle_exception(TechmanException(CPERR_packet(res).description))
+         self._handle_incoming(res)
 
    @property
    def is_connected(self): return self._conn_exception is None
@@ -104,13 +119,13 @@ if __name__ == "__main__":
       else: print(f'received: {res.encoded()}')
       asyncio.get_event_loop().stop()
 
-   clnt = TechmanClient(robot_ip='localhost', robot_port=5890)
-   clnt.send(TMSTA_packet(TMSTA_type.QUEUE_TAG, [1]), callback=callback)
+   clnt = TechmanClient(robot_ip='10.66.0.117', robot_port=5891)
+   # clnt.send(TMSTA_packet(TMSTA_type.QUEUE_TAG, [1]), callback=callback)
    asyncio.get_event_loop().run_forever()
 
-   clnt._robot_ip='10.66.0.117'
+   # clnt._robot_ip='10.66.0.117'
 
-   try:
-      packet = clnt.send(TMSTA_packet(TMSTA_type.QUEUE_TAG, [1]))
-      print(f'received: {packet.encoded()}')
-   except TechmanException as e: print(type(e).__name__ + ': ' + str(e))
+   # try:
+   #    packet = clnt.send(TMSTA_packet(TMSTA_type.QUEUE_TAG, [1]))
+   #    print(f'received: {packet.encoded()}')
+   # except TechmanException as e: print(type(e).__name__ + ': ' + str(e))
--- clients/tmsvr_client.py
+++ clients/tmsvr_client.py
@@ -19,9 +19,29 @@ class TMSVR_client(TechmanClient):
    PORT=5891
 
    def __init__(self, broadcast_callback=None, *, robot_ip, id):
-      super(TMSVR_client, self).__init__(robot_ip=robot_ip, robot_port=self.PORT, broadcast_callback=broadcast_callback)
+      super(TMSVR_client, self).__init__(robot_ip=robot_ip, robot_port=self.PORT)
+      self._broadcast_callback = broadcast_callback
       self._id = str(id)
       self._msg_cnt = 0
+
+   def _handle_incoming(self, packet):
+      packet = TMSVR_packet(packet)
+      if len(packet.handle_id) == 1:
+         self._broadcast_callback(packet)
+         return
+      index = -1
+      callback = None
+      for i, request in enumerate(self._outgoing_requests):
+         if request[0] != packet.handle_id: continue
+         index = i
+         callback = request[1]
+      assert index != -1
+      del self._outgoing_requests[index]
+      # Execute callback with received packet
+      callback(packet)
+
+   def _handle_outgoing(self, packet, callback):
+      self._outgoing_requests.append((packet.handle_id, callback))
 
    def get_values(self, items):
       # Build TMSVR packet
@@ -47,12 +67,9 @@ class TMSVR_client(TechmanClient):
       handle_id = '%s%d' % (self._id, self._msg_cnt)
       self._msg_cnt += 1
       req = TMSVR_packet(handle_id, TMSVR_type.WRITE_REQUEST, items)
-      print(req.encoded())
       # Submit
       res = TMSVR_packet(self.send(req))
       # Parse response
-      print(res.handle_id)
-      print(handle_id)
       assert res.handle_id == handle_id
       assert res.ptype == TMSVR_type.WRITE_RESPONSE
       if res.status != TMSVR_status.SUCCESS:
@@ -61,33 +78,18 @@ class TMSVR_client(TechmanClient):
    def set_value(self, key, value):
       return self.set_values({key: value})
 
-   def listen(self): return asyncio.get_event_loop().run_until_complete(self._listen_async())
-   async def _listen_async(self):
-      if not self._is_connected:
-         if await self._connect_async(): self._is_connected = True
-         else: raise TechmanException('Could not connect to robot at %s:%s' % (self._robot_ip, self._robot_port))
-      read_bytes = await self._reader.read(1024)
-      if read_bytes == b'':
-         if self._reconnect_cnt > 5:
-            self._reconnect_cnt = 0
-            raise TechmanException('Could not connect to robot at %s:%s' % (self._robot_ip, self._robot_port))
-         self._reconnect_cnt += 1
-         self._is_connected = False
-         return await self._send_async(techman_packet)
-      else: 
-         self._reconnect_cnt = 0
-         res = TechmanPacket(read_bytes)
-         if res._header == 'CPERR':
-            raise TechmanException(CPERR_packet(res).description)
-         else: return res
-
 
 if __name__ == "__main__":
    def broadcast_callback(packet):
-      print(f'Received {packet._header} packet')
+      print(f'Received {packet._header} packet with id {packet.handle_id}')
+      #print('blokkie nee')
+      #print(packet.items)
+      #print('blokkie huh')
    clnt = TMSVR_client(robot_ip='10.66.0.117', id='DC', broadcast_callback=broadcast_callback)
-   try:
-      print(clnt.listen().encoded())
-      clnt.set_value('Camera_Light', 1)
+   #try:
+      #clnt.set_value('Camera_Light', 0)
       # print(clnt.get_value('Camera_Light'))
-   except TechmanException as e: print(type(e).__name__ + ': ' + str(e))
+   #except TechmanException as e: print(type(e).__name__ + ': ' + str(e))
+
+   asyncio.get_event_loop().run_forever()
+   print('nee toch')
